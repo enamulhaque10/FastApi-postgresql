0d9c56a49c7d9e2a56151f7e8b4eff9d870fca9e
shawon <enamul@eatlbd.com>
2023-09-07 11:18:34 +0600

    chnges db url

--- database.py
+++ database.py
@@ -2,7 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-URL_DATABASE = 'postgresql://postgres:postgres123@localhost:5432/postgres'
+URL_DATABASE = 'postgres://fastapi_9kej_user:y8NTBeTo0FKqa2v0cAFAxkRlfgzQRhd4@dpg-cjslmum8b8as73d5lfhg-a.oregon-postgres.render.com/fastapi_9kej'
+# 'postgresql://postgres:postgres123@localhost:5432/postgres'
 
 engine = create_engine(URL_DATABASE)
 
