b455d1046d61b88eb760df5fea1eeeac18f67af3
enamul <enamul@eatlbd.com>
2023-09-10 21:59:15 +0600

    changes url

--- database.py
+++ database.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# URL_DATABASE = 'postgresql://fastapi_9kej_user:y8NTBeTo0FKqa2v0cAFAxkRlfgzQRhd4@dpg-cjslmum8b8as73d5lfhg-a:5432/fastapi_9kej'
-URL_DATABASE = 'postgresql://postgres:postgres123@localhost:5432/postgres'
+URL_DATABASE = 'postgresql://fastapi_9kej_user:y8NTBeTo0FKqa2v0cAFAxkRlfgzQRhd4@dpg-cjslmum8b8as73d5lfhg-a:5432/fastapi_9kej'
+#URL_DATABASE = 'postgresql://postgres:postgres123@localhost:5432/postgres'
 
 
 engine = create_engine(URL_DATABASE)
